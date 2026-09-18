2fc0236e1110bca769e5730fdc4598f7e9e3c916
AanyaaAgarwal <aanyaagarwal301007@gmail.com>
2026-09-18 15:14:15 +0530

    Update at_content_calculator.py

--- at_content_calculator.py
+++ at_content_calculator.py
@@ -5,6 +5,6 @@ dna_length = len(dna)
 count_a = dna.count("A")
 count_t = dna.count("T")
 
-at_content = (count_a + count_t) / dna_length
+at_content = ((count_a + count_t) / dna_length)*100
 
-print("The AT content is " + str(at_content))
+print("The AT content is ", at_content, "%")
